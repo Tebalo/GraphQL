f4ea42ba437c7947ac767e279b67b5f33fcd208c
Tebalo <btebalo@gmail.com>
2023-02-17 23:33:45 +0200

    Created A Data Access Layer

--- db.js
+++ db.js
@@ -1,3 +1,8 @@
 const { DataStore } = require('notarealdb');
 
 const store = new DataStore('./data');
+
+module.exports = {
+    students: store.collection('students'),
+    colleges: store.collection('colleges')
+};
