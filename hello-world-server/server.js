920a81a7ac08c33efff6b6b72dbf3082f0276315
Tebalo <btebalo@gmail.com>
2023-02-23 00:37:36 +0200

    outdated libraries

--- hello-world-server/server.js
+++ hello-world-server/server.js
@@ -6,6 +6,7 @@ const app = express()
 
 //register middleware
 app.use(bodyParser.json(), cors())
+//app.listen(port, () => console.log(`server is up and running at ${port}`))
 
 /** 
  * Define the Schema
@@ -15,8 +16,8 @@ app.use(bodyParser.json(), cors())
  */
 //Adding Type Definitions
 const typeDefinition = `
-type Query {
-greeting: String
+    type Query {
+        greeting: String
 }`
 /**
  * Adding a resolver
@@ -26,6 +27,7 @@ const resolverObject = {
         greeting: () => 'Hello GraphQL From TutorialsPoint !!'
     }
 }
+
 /**
  * Bind the schema and the resolver
  */
@@ -33,6 +35,7 @@ const { makeExecutableSchema } = require('graphql-tools')
 const schema = makeExecutableSchema({ typeDefs: typeDefinition, resolvers: resolverObject })
 
 const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+// create routes for graphql and graphical
 app.use('/graphql', graphqlExpress({ schema }))
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 app.listen(port, () => console.log(`server is up and running ${port}`))
