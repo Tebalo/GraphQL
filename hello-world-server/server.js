02899775b0ebecbc2f859b321a5bbc9c9edf158b
Tebalo <btebalo@gmail.com>
2023-02-22 23:25:13 +0200

    fixed the error

--- hello-world-server/server.js
+++ hello-world-server/server.js
@@ -7,3 +7,15 @@ const app = express()
 //register middleware
 app.use(bodyParser.json(), cors())
 app.listen(port, () => console.log(`server is up and running at ${port}`))
+
+/** 
+ * Define the Schema
+ * A GraphQL schema defines what kind of object can be fetched from a service, and
+ * what fields it has. The schema can be defined using GraphQL Schema Definition 
+ * Language.
+ */
+//Adding Type Definitions
+const typeDefinition = gql`
+    type Query {
+        greeting: String
+}`;
